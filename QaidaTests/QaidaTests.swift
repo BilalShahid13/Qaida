e6db003b282be80909c61948cf0a3f48dfaa244b
BilalShahid13 <bilalscheema@gmail.com>
2019-06-11 19:57:27 +0500

    Initial Commit

---
+++ QaidaTests/QaidaTests.swift
@@ -0,0 +1,34 @@
+//
+//  QaidaTests.swift
+//  QaidaTests
+//
+//  Created by Bilal Shahid on 6/11/19.
+//  Copyright © 2019 Bilal Shahid. All rights reserved.
+//
+
+import XCTest
+@testable import Qaida
+
+class QaidaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
